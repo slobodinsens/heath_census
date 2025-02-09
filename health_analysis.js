84c9cc727977187b2891e7cc7fec1c5de2f34274
slobodinsens <dmitry@sensfusion.ai>
2025-02-09 10:08:22 +0200

    Update health_analysis.js

---
+++ health_analysis.js
@@ -0,0 +1,56 @@
+const addPatientButton = document.getElementById("addPatient");
+const report = document.getElementById("report");
+const btnSearch = document.getElementById('btnSearch');
+const patients = [];
+
+function addPatient() {
+    const name = document.getElementById("name").value;
+    const gender = document.querySelector('input[name="gender"]:checked');
+    const age = document.getElementById("age").value;
+    const condition = document.getElementById("condition").value;
+
+    if (name && gender && age && condition) {
+      patients.push({ name, gender: gender.value, age, condition });
+      resetForm();
+      generateReport();
+    }
+  }
+
+  function resetForm() {
+    document.getElementById("name").value = "";
+    document.querySelector('input[name="gender"]:checked').checked = false;
+    document.getElementById("age").value = "";
+    document.getElementById("condition").value = "";
+  }
+
+  function searchCondition() {
+    const input = document.getElementById('conditionInput').value.toLowerCase();
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = '';
+
+    fetch('health_analysis.json')
+      .then(response => response.json())
+      .then(data => {
+        const condition = data.conditions.find(item => item.name.toLowerCase() === input);
+
+        if (condition) {
+          const symptoms = condition.symptoms.join(', ');
+          const prevention = condition.prevention.join(', ');
+          const treatment = condition.treatment;
+
+          resultDiv.innerHTML += `<h2>${condition.name}</h2>`;
+          resultDiv.innerHTML += `<img src="${condition.imagesrc}" alt="hjh">`;
+
+          resultDiv.innerHTML += `<p><strong>Symptoms:</strong> ${symptoms}</p>`;
+          resultDiv.innerHTML += `<p><strong>Prevention:</strong> ${prevention}</p>`;
+          resultDiv.innerHTML += `<p><strong>Treatment:</strong> ${treatment}</p>`;
+        } else {
+          resultDiv.innerHTML = 'Condition not found.';
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+        resultDiv.innerHTML = 'An error occurred while fetching data.';
+      });
+  }
+    btnSearch.addEventListener('click', searchCondition);
